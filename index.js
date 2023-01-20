ac95d1c68ea2b2ec807eb0d1abc5d2fbe0ef897c
ezechukwu nwokedi <israellambert96@gmail.com>
2023-01-20 19:49:14 +0000

    user choice now works, added license helper

--- index.js
+++ index.js
@@ -5,32 +5,48 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 const markdownSections = [];
 
+const getLicenseBadge = (license) =>{
+    switch(license){
+        case "MIT": return `![MIT License](link-to-badge)`
+        case "Apache": return `![Apache License](link-to-badge)`
+        case "GNU": return `![GNU License](link-to-badge)`
+        case "MIT": return `![MIT](link-to-badge)`
+        default: "No License"
+    }
+}
+
+// this function expression gives the user the option of choosing which
+// optional sections to add to the file
 const userSectionChoice = () => {
-    console.log(`This is a dynamic README.md file generator. Users however may want not want some of the opptional 
-                sections so these could bbe removed. Please select the optional sections you would like to add to the README.md: `)
-    inquirer.createPromptModule([
+    console.log(`This is a dynamic README.md file generator. Users however may want not want some of the optional sections so these could be removed. Please select the optional sections you would like to add to the README.md: `)
+    inquirer.prompt([
         {
             type: 'checkbox',
             message: 'What optional sections would you like to add?',
-            name: '',
-            values:["Table of Content","Recommended Modules", "Troubleshooting & FAQ"]
+            name: 'optionals',
+            choices:["Table of Content","Recommended Modules", "Troubleshooting & FAQ", "License"]
         },
     ]).then((response)=>{
+        //console.log(response)
+        // Compile list of all compulsory and user-picked optional headings in order 
         markdownSections.push("Project Name")
+        if (response.optionals.includes("License")){
+            markdownSections.push("License")
+        }
         markdownSections.push("Introduction")
-        if ("Table of Content" in Object.values(response)){
+        if (response.optionals.includes("Table of Content")){
             markdownSections.push("Table of Content")
         }
         markdownSections.push("Requirements")
-        if ("Recommended Modules" in Object.values(response)){
+        if (response.optionals.includes("Recommended Modules")){
             markdownSections.push("Recommended Modules")
         }
         markdownSections.push("Installation")
         markdownSections.push("Configuration")
-        if ("Troubleshooting & FAQ" in Object.values(response)){
+        if (response.optionals.includes("Troubleshooting & FAQ")){
             markdownSections.push("Troubleshooting & FAQ")
         }
-        console.log("User sections compiled!")
+        console.log("User sections compiled: ", markdownSections)
     })
 }
 
