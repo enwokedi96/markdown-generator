19ccd6231e4f557e02d67dcd846e2d4a190aa9f6
ezechukwu nwokedi <israellambert96@gmail.com>
2023-01-20 19:13:06 +0000

    dependency install done, codes for optionals done

--- index.js
+++ index.js
@@ -3,6 +3,39 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+const markdownSections = [];
+
+const userSectionChoice = () => {
+    console.log(`This is a dynamic README.md file generator. Users however may want not want some of the opptional 
+                sections so these could bbe removed. Please select the optional sections you would like to add to the README.md: `)
+    inquirer.createPromptModule([
+        {
+            type: 'checkbox',
+            message: 'What optional sections would you like to add?',
+            name: '',
+            values:["Table of Content","Recommended Modules", "Troubleshooting & FAQ"]
+        },
+    ]).then((response)=>{
+        markdownSections.push("Project Name")
+        markdownSections.push("Introduction")
+        if ("Table of Content" in Object.values(response)){
+            markdownSections.push("Table of Content")
+        }
+        markdownSections.push("Requirements")
+        if ("Recommended Modules" in Object.values(response)){
+            markdownSections.push("Recommended Modules")
+        }
+        markdownSections.push("Installation")
+        markdownSections.push("Configuration")
+        if ("Troubleshooting & FAQ" in Object.values(response)){
+            markdownSections.push("Troubleshooting & FAQ")
+        }
+        console.log("User sections compiled!")
+    })
+}
+
+userSectionChoice()
+
 // array of questions for user
 const questions = [
 
